aba53e200f9f8ac548a28469306c703de3aabf7d
joaocneves7 <jgcavalcantineves@gmail.com>
2025-04-29 22:30:55 -0300

    criando subtraçao

---
+++ subtraçao.py
@@ -0,0 +1,5 @@
+print("Isso é uma calculadora!")
+ num1 = int(input("Digite o primeiro numero: "))
+ num2 = int(input("Digite o segundo numero: "))
+ resultado = num1 - num2
+ print(f"Resultado: {resultado}")
