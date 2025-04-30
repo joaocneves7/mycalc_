54cfc5f06a6121d85df6d20345bf2ea8b376ef00
joaocneves7 <jgcavalcantineves@gmail.com>
2025-04-29 22:32:51 -0300

    criando divisao

---
+++ divisao.py
@@ -0,0 +1,5 @@
+print("Isso é uma calculadora!")
+ num1 = float(input("Digite o primeiro numero: "))
+ num2 = float(input("Digite o segundo numero: "))
+ resultado = num1 / num2
+ print(f"Resultado: {resultado}")
